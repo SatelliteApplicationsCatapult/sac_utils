41d755c4c7242070bf99a40bbd814dc795692c1d
Luigi Di Fraia <luigidifraia@users.noreply.github.com>
2020-02-10 15:37:36 +0000

    Process requirements

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open('requirements.txt') as requirements_file:
+    requirements = [line.rstrip('\n') for line in requirements_file]
+
 setuptools.setup(
     name="sac_utils",
     version="0.0.1",
@@ -13,6 +16,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/SatelliteApplicationsCatapult/sac_utils",
     packages=setuptools.find_packages(include=['sac_utils']),
+    install_requires=requirements,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
